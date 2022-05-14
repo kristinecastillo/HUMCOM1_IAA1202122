37c0cd98f5a11b1e0a87272b81623a0575b08806
Kristine Castillo <97872997+kristinecastillo@users.noreply.github.com>
2022-05-14 20:10:24 +0800

    Delete FinalExam_Castillo.js

--- Final Exam-Project/javascript/FinalExam_Castillo.js
+++
@@ -1,9 +0,0 @@
-const menuBtn = document.querySelector('.menu-btn')
-const navlinks = document.querySelector('.nav-link')
-
-menuBtn.addEventListener('click', ()=>{
-    navlinks.classList.toggle('mobile-menu')
-})
-
-console.log("Author: Kristine Coloma Castillo")
-console.log("1st Year College - Bachelor of Science in Computer Science")
